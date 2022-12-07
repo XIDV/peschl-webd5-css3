7a376e45f64fb21c1a0059e72bd3fccb25d73a62
Sebastian Peschl <sebastian.peschl@xidv.de>
2022-12-07 17:20:29 +0100

    verbesserungen, bugfix an pageNav bzg. responsivity + verbesserungen an toggleICCB()

--- script/script.js
+++ script/script.js
@@ -10,27 +10,27 @@ document.addEventListener('DOMContentLoaded', dcl => {
     navButton.addEventListener('click', e => toggleMenu());
     navButton.addEventListener('touchstart', e => toggleMenu());
 
-    iccb.addEventListener('click', e => toggleICCB('mouse'));
-    iccb.addEventListener('touchstart', e => toggleICCB('touch'));
+    iccb.addEventListener('click', e => toggleICCB());
+    iccb.addEventListener('touchstart', e => toggleICCB());
 
+    window.addEventListener('resize', e => setPageNav())
+
+    function setPageNav() {
+        window.innerWidth >= 1024 ? pageNav.style.height = '3rem' : pageNav.style.height = '0';
+    }
 
     let navStatus = false;
     function toggleMenu() {
-        if(!navStatus) {
-            pageNav.style.height = '25rem';
-        } else {
-            pageNav.style.height = '0';
+        if(window.innerWidth < 1024) {
+            !navStatus ? pageNav.style.height = '25rem' : pageNav.style.height = '0';
+            navStatus = !navStatus;
+
         }
-        navStatus = !navStatus;
     }
 
     let iccbStatus = false;
-    function toggleICCB(typeofevent) {
-        if(!iccbStatus) {
-            icc.style.right = '0';
-        } else {
-            icc.style.right = '-15rem';
-        }
+    function toggleICCB() {
+        !iccbStatus ? icc.style.right = '0' : icc.style.right = '-15rem';
         iccbStatus = !iccbStatus;
     }
 });
