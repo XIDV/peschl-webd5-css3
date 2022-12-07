21567a43e937995fdb60671233ee50aab1004aeb
Sebastian Peschl <sebastian.peschl@xidv.de>
2022-12-07 11:17:12 +0100

    pageNav gestylt + ein/ausbelenden mit button impl

--- script/script.js
+++ script/script.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded', dcl => {
 
+    const navButton = document.querySelector('#navButton');
+    const pageNav = document.querySelector('#pageNav');
 
     const iccb = document.querySelector('#impCSwitch');
     const icc = document.querySelector('#importantContainer');
 
+
+    navButton.addEventListener('click', e => toggleMenu());
+
     iccb.addEventListener('click', e => toggleICCB('mouse'));
     iccb.addEventListener('touchstart', e => toggleICCB('touch'));
 
+
+    let navStatus = false;
+    function toggleMenu() {
+        if(!navStatus) {
+            pageNav.style.height = '25rem';
+        } else {
+            pageNav.style.height = '0';
+        }
+        navStatus = !navStatus;
+    }
 
     let iccbStatus = false;
     function toggleICCB(typeofevent) {
