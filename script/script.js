0e0660d25c8c9cf2e356b816696f6fb16d82f1fa
Sebastian Peschl <sebastian.peschl@xidv.de>
2022-12-11 08:53:27 +0100

    Korrekturen / Ergänzungen

--- script/script.js
+++ script/script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', dcl => {
     function setDynamicElements() {
         const innerWidth = window.innerWidth;
         if(innerWidth >= 1024) {
-            pageNav.style.height = '3rem';
+            pageNav.style.height = '2.5rem';
             loginContainer.open = true;
             pageFooter.style.position = 'sticky';
             pageFooter.style.bottom = '0';
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', dcl => {
     }
 
     let navStatus = false;
-    function toggleMenu() {
+    function toggleMenu(e) {
         if(window.innerWidth < 1024) {
             !navStatus ? pageNav.style.height = '25rem' : pageNav.style.height = '0';
             navStatus = !navStatus;
